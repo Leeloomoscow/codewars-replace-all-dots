60900a8ed89ad3d83b3d26f27bec99b9ef72253a
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-10-20 15:58:45 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,16 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MyTest {
     @Test
-    void name() {
-
+    public void testSomeDots() {
+        assertTrue("one-two-three".equals(Dinglemouse.replaceDots("one.two.three")), "Sorry, try again :-(");
     }
-}
+
+    private static class Dinglemouse {
+        static String replaceDots(final String str) {
+            return str.replace(".", "-");
+        }
+    }
+    }
